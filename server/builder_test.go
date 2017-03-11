8ae89a0cdd2a04573cf55c77ee738d6f17479171
deadcheat <feat.dead.cheat+github@gmail.com>
2017-03-11 23:43:16 +0900

    add a part passing values from forwardHeaders to request header to func buildHttpRequest

--- server/builder_test.go
+++ server/builder_test.go
@@ -47,6 +47,8 @@ func TestBuildHttpRequest(t *testing.T) {
 
 	headers := make(http.Header)
 	headers.Add("X-Forwarded-For", "127.0.0.1")
+	headers.Add("X-Auth-Token", "Bearer test_token")
+	headers.Add("X-Auth-Token2", "Bearer another_testtoken")
 	for _, reqj := range reqjs {
 		reqh, err := buildHttpRequest(&reqj, &headers)
 		assert.Nil(err)
@@ -60,12 +62,14 @@ func TestBuildHttpRequest(t *testing.T) {
 	assert.Equal("127.0.0.1:30001", reqhs[0].URL.Host)
 	assert.Equal("/user/get", reqhs[0].URL.Path)
 	assert.Equal("user_id=1", reqhs[0].URL.RawQuery)
+	assert.Equal("Bearer test_token", reqhs[0].Header.Get("Authorization"))
 
 	assert.Equal("", reqhs[1].Header.Get("Content-Type"))
 	assert.Equal("ep1.example.com", reqhs[1].Host)
 	assert.Equal("127.0.0.1:30001", reqhs[1].URL.Host)
 	assert.Equal("/item/get", reqhs[1].URL.Path)
 	assert.Equal("item_id=2", reqhs[1].URL.RawQuery)
+	assert.Equal("Bearer test_token", reqhs[1].Header.Get("Authorization"))
 
 	buf.ReadFrom(reqhs[2].Body)
 	assert.Equal("application/x-www-form-urlencoded", reqhs[2].Header.Get("Content-Type"))
@@ -73,4 +77,5 @@ func TestBuildHttpRequest(t *testing.T) {
 	assert.Equal("127.0.0.1:30002", reqhs[2].URL.Host)
 	assert.Equal("/item/update", reqhs[2].URL.Path)
 	assert.Equal("desc=update&item_id=2", buf.String())
+	assert.Equal("Bearer another_testtoken", reqhs[2].Header.Get("Authorization"))
 }
